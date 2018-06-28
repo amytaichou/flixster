4ad5fb9b263a8f0f99cccbdd67e8e26e2ba0b083
Amy Liu <maymayliu@fb.com>
2018-06-28 10:34:29 -0700

    Initial Commit

---
+++ flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flixster
+//
+//  Created by Amy Liu on 6/28/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flixster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  flixster
+//
+//  Created by Amy Liu on 6/28/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
